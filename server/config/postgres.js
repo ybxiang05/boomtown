666535545dbeb4e8662f0e01e73d8d4c9f0dad5f
Brooke <brooke.xiang@gmail.com>
2020-01-21 16:31:37 -0800

    init

---
+++ server/config/postgres.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+
+module.exports = app => {
+  /**
+   * @TODO: Configuration Variables
+   *
+   *  Retrieve the necessary information to connect to Postgres
+   *  For example: app.get('PG_DB')
+   */
+  return new Pool({
+    host: app.get("PG_HOST"),
+    user: app.get("PG_USER"),
+    password: app.get("PG_PASSWORD"),
+    database: app.get("PG_DB"),
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+  });
+};
