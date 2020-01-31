c0d9056150dc874f173bd20832f95e706a496e3b
Brooke <brooke.xiang@gmail.com>
2020-01-31 15:35:22 -0800

    return string if (loading) to prevent returning undefined

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import Items from './Items';
+import React, { Component } from "react";
+import Items from "./Items";
 // import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import { } from '../../apollo/queries';
+import { Query } from "react-apollo";
+import {} from "../../apollo/queries";
+import { ALL_ITEMS_QUERY } from "../../apollo/queries";
 
 class ItemsContainer extends Component {
   render() {
-    return <Items />;
+    return (
+      <Query query={ALL_ITEMS_QUERY} variables={{ filter: 2 }}>
+        {({ loading, error, data }) => {
+          if (loading) return "loading";
+          //console.log(data);
+          // if (loading) return <FullScreenLoader />;
+          if (error) return `Error! ${error.message}`;
+          return <Items items={data.items} />;
+        }}
+      </Query>
+    );
   }
 }
 
