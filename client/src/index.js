6aa4069a96d444ff7d779517fd93847aed0ff713
Brooke <brooke.xiang@gmail.com>
2020-01-31 15:39:20 -0800

    wrap AppRoutes in Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,13 @@
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes";
+import client from "./apollo";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 import ReactDOM from "react-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-// @TODO: Uncomment each module as needed in your client app
-// import { ApolloProvider } from 'react-apollo'
-// import { BrowserRouter } from 'react-router-dom'
-// -------------------------------
-
 import registerServiceWorker from "./registerServiceWorker";
 import theme from "./theme";
-
-/**
- * @TODO: Initialize Apollo Client
- *
- * Uncomment the following line when Apollo Client is configured:
- *
- * import client from './apollo'
- *
- * Below in your <App />, wrap your pages in an <ApolloProvider /> component
- * and pass it `client` as the `client` prop value so they will
- * have access to data exposed by your GraphQL API.
- */
-
-/**
- * @TODO: Add Routing
- *
- * Uncomment the following line when your routes are configured
- *
- * import AppRoutes from './routes'
- *
- * Below in your <App />, nest your <AppRoutes /> inside of <BrowserRouter />
- * component to enable routing in your client app.
- */
 
 /**
  * @TODO: Wrap your app with the Item Preview Provider
@@ -52,7 +28,7 @@ import theme from "./theme";
  */
 
 // @TODO: Remove this import once you have your router working below
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 // -------------------------------
 
 import "./index.css";
@@ -61,7 +37,11 @@ const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Home />
+      <ApolloProvider client={client}>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </ApolloProvider>
     </MuiThemeProvider>
   );
 };
