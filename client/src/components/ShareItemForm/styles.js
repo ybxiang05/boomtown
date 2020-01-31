c6fd780830721274c892b28900408ebc5ff5aab8
Brooke <brooke.xiang@gmail.com>
2020-01-31 15:44:31 -0800

    add styling to share form

--- client/src/components/ShareItemForm/styles.js
+++ client/src/components/ShareItemForm/styles.js
@@ -1,3 +1,11 @@
-const styles = theme => ({});
+import { createStyles } from "@material-ui/core";
+
+const styles = createStyles({
+  formContainer: {
+    marginTop: "200px",
+    display: "flex",
+    flexFlow: "column wrap"
+  }
+});
 
 export default styles;
