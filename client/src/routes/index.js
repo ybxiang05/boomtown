a41a9eb404c71f06c791372bd8dddfc6ee6741dd
Brooke <brooke.xiang@gmail.com>
2020-01-31 15:29:47 -0800

    add routes to site

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,20 +1,23 @@
 import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Items from "../pages/Items/Items";
-import Profile from "../pages/Profile/Profile";
-import Share from "../pages/Share/Share";
+import Items from "../pages/Items";
+import Profile from "../pages/Profile";
+import Share from "../pages/Share";
+import Home from "../pages/Home";
+import NavBar from "../components/NavBar/NavBar.js";
 
 export default () => (
   <Fragment>
-    {/* @TODO: Add your menu component here */}
+    {/* @TODO: Add your menu component here - this will be NavLink? */}
+    <NavBar />
     <Switch>
       {/**
        * Later, we'll add logic to send users to one set of routes if they're logged in,
        * or only view the /welcome page if they are not.
        */}
-      <Route exact path="/items">
-        <Items />
-      </Route>
+      <Route exact path="/items" component={Items} />
+      <Route exact path="/home" component={Home} />
+
       <Route exact path="/profile" component={Profile} />
       <Route exact path="/profile/:id" component={Profile} />
       <Route exact path="/share" component={Share} />
