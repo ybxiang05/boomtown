96173eb3222ad3bd9e8778feaf8aed2aa4e374da
Brooke <brooke.xiang@gmail.com>
2020-01-31 15:43:19 -0800

    add form fields for share page

--- client/src/components/ShareItemForm/ShareItemForm.js
+++ client/src/components/ShareItemForm/ShareItemForm.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { withStyles, CheckBox, TextField } from "@material-ui/core/";
+import styles from "./styles";
+import { Form, Field } from "react-final-form";
 
-class ShareForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+class ShareItemForm extends Component {
+  onSubmit = values => {
+    console.log(values);
+  };
+  validate = values => {
+    const errors = {};
+    console.log(values);
+  };
 
   render() {
+    console.log(this.props);
+    const { classes } = this.props;
     return (
-      <div>
-        <p>This is the share form.</p>
-      </div>
+      <Form
+        onSubmit={this.onSubmit}
+        validate={this.validate}
+        render={({ handleSubmit }) => {
+          return (
+            <form onSubmit={handleSubmit} className={classes.formContainer}>
+              <Field
+                name="item name"
+                type="textarea"
+                render={({ input, meta }) => (
+                  <React.Fragment>
+                    <TextField {...input} placeholder="Name your item." />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </React.Fragment>
+                )}
+              />
+              <Field
+                name="item description"
+                type="text"
+                render={({ input, meta }) => (
+                  <React.Fragment>
+                    <TextField {...input} placeholder="Describe your item." />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </React.Fragment>
+                )}
+              />
+            </form>
+          );
+        }}
+      ></Form>
     );
   }
 }
 
-export default ShareForm;
+export default withStyles(styles)(ShareItemForm);
