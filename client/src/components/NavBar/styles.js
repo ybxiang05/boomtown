a2f8e4996c6fd41745427679236fc52349bc311a
Brooke <brooke.xiang@gmail.com>
2020-02-03 16:33:26 -0800

    add style to navbar

---
+++ client/src/components/NavBar/styles.js
@@ -0,0 +1,32 @@
+import { createStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    // marginRight: theme.spacing(2)
+  },
+  navLogo: {
+    height: "50px"
+  },
+  title: {
+    flexGrow: 1
+  },
+  link: {
+    color: "black",
+    alignItems: "center"
+  },
+  toolBar: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  shareButton: {
+    textTransform: "uppercase"
+  },
+  circIcon: {
+    transform: "translateY(7px)"
+  }
+});
+
+export default styles;
