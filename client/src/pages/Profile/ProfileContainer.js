33fdecc104ee0a7fc95a344c7b566598bf19bed9
Brooke <brooke.xiang@gmail.com>
2020-02-07 13:10:40 -0800

    add routing for profiles

--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -8,18 +8,24 @@ import FullScreenLoader from "../../components/FullScreenLoader";
 class ProfileContainer extends Component {
   render() {
     return (
-      <ViewerContext.Consumer>
-        {({ viewer, data }) => (
-          <Query query={ALL_USER_ITEMS_QUERY} variables={{ id: viewer.id }}>
-            {({ loading, error, data }) => {
-              if (loading) return <FullScreenLoader />;
-              if (error) return `error, ${error.message}`;
-              if (data) return <Profile data={data} />;
-            }}
-            {/* {console.log(data)} */}
-          </Query>
-        )}
-      </ViewerContext.Consumer>
+      console.log(this.props),
+      (
+        <ViewerContext.Consumer>
+          {({ viewer, data }) => (
+            <Query
+              query={ALL_USER_ITEMS_QUERY}
+              variables={{ id: this.props.match.params.id || viewer.id }}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <FullScreenLoader />;
+                if (error) return `error, ${error.message}`;
+                if (data) return <Profile data={data} />;
+              }}
+              {/* {console.log(data)} */}
+            </Query>
+          )}
+        </ViewerContext.Consumer>
+      )
     );
   }
 }
