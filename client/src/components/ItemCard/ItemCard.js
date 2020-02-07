8bdd1c69d87c4aab856a554355a7ef0a24ca5128
Brooke <brooke.xiang@gmail.com>
2020-02-07 14:33:40 -0800

    add condition to display gravatar

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import moment from "moment";
-import { NavLink } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Gravatar from "react-gravatar";
+import { ViewerContext } from "../../context/ViewerProvider";
 
 import {
   Card,
@@ -18,61 +19,65 @@ import {
   Typography
 } from "@material-ui/core/";
 
-const ItemCard = ({ item, classes }) => {
-  // console.log("yes?", item);
+const ItemCard = ({ item, classes, location }) => {
+  console.log("yes?", location);
   // const { tags } = item;
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <NavLink to={`/profile/${item && item.itemowner.id}`}>
-          <CardMedia
-            component="img"
-            alt={item && item.title}
-            height="225"
-            image=" http://place-puppy.com/200x200"
-          />
-        </NavLink>
-        <CardContent>
-          <Box className={classes.userInfo}>
-            <Gravatar
-              email={item && item.itemowner && item.itemowner.email}
-              className={classes.gravatar}
-            />
-            <div>
-              <Typography variant="body1">
-                {item && item.itemowner && item.itemowner.fullname}
+    <ViewerContext.Consumer>
+      {({ viewer, loading }) => (
+        <Card className={classes.card}>
+          <CardActionArea>
+            <Link to={item && item.itemowner.id && `/profile/${item && item.itemowner.id}`}>
+              <CardMedia
+                component="img"
+                alt={item && item.title}
+                height="225"
+                image={item.imageurl ? item.imageurl : "http://place-puppy.com/200x200"}
+              />
+            </Link>
+            <CardContent>
+              <Box className={classes.userInfo}>
+                <Gravatar
+                  email={(item && item.itemowner && item.itemowner.email) || viewer.email}
+                  className={classes.gravatar}
+                />
+                <div>
+                  <Typography variant="body1">
+                    {(item && item.itemowner && item.itemowner.fullname) || viewer.fullname}
+                  </Typography>
+                  <p variant="body1" color="textSecondary" component="p">
+                    {moment(item && item.created).fromNow()}
+                  </p>
+                </div>
+              </Box>
+
+              <Typography gutterBottom variant="h5" component="h2">
+                {item && item.title}
               </Typography>
-              <p variant="body1" color="textSecondary" component="p">
-                {moment(item && item.created).fromNow()}
-              </p>
-            </div>
-          </Box>
 
-          <Typography gutterBottom variant="h5" component="h2">
-            {item && item.title}
-          </Typography>
-
-          {item &&
-            item.tags &&
-            item.tags.map(tag => {
-              return (
-                <Typography variant="body1" color="textSecondary" key={tag.id}>
-                  {tag.title}{" "}
-                </Typography>
-              );
-            })}
-          <Typography variant="body1" component="p">
-            {item && item.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Box className={classes.borrowButton}>
-          <Button>Borrow</Button>
-        </Box>
-      </CardActions>
-    </Card>
+              {item &&
+                item.tags &&
+                item.tags.map(tag => {
+                  return (
+                    <Typography variant="body1" color="textSecondary" key={tag.id}>
+                      {tag.title}{" "}
+                    </Typography>
+                  );
+                })}
+              <Typography variant="body1" component="p">
+                {item && item.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Box className={classes.borrowButton}>
+              <Button>Borrow</Button>
+            </Box>
+          </CardActions>
+        </Card>
+      )}
+    </ViewerContext.Consumer>
   );
 };
 
-export default withStyles(styles)(ItemCard);
+export default withRouter(withStyles(styles)(ItemCard));
