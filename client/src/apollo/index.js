4c7f1d330a1e8d57a9773ea565b72ab02b4a037d
Brooke <brooke.xiang@gmail.com>
2020-03-09 15:41:56 -0700

    remove extreneous code

--- client/src/apollo/index.js
+++ client/src/apollo/index.js
@@ -15,9 +15,11 @@ const client = new ApolloClient({
     onError(({ graphQLErrors, networkError }) => {
       // Log better error messages to console
       if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-        );
+        graphQLErrors.map(({ message, locations, path }) => {
+          return console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          );
+        });
       }
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
